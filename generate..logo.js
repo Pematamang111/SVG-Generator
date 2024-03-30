0a03996a0daa54863ff6037b7c7f36637a52ed06
Pema Sherpa <pematamang111@gmail.com>
2024-03-30 16:51:07 -0400

    add data.shape

--- generate..logo.js
+++ generate..logo.js
@@ -3,11 +3,11 @@ const { Circle, Square, Triangle } = require('./lib/shapes.js');
 
 function generateLogo(data){
 let shape = data.shape;
-if (data.shape === Circle) {
+if (data.shape === 'Circle') {
     shape = new Circle(data.color, data.text, data.textColor);
-}else if (data.shape === Square) {
+}else if (data.shape === 'Square') {
     shape = new Square(data.color, data.text, data.textColor);
-}else if (data.shape === Triangle) {
+}else if (data.shape === 'Triangle') {
     shape = new Triangle(data.color, data.text, data.textColor);
 }
 return shape.render();
