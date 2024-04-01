d6d2a27b083aca4774e0c589643975e079809a6a
Pema Sherpa <pematamang111@gmail.com>
2024-04-01 17:12:40 -0400

    add svg in test

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -6,6 +6,7 @@ describe('Shapes', () =>{
     describe('instance', () => {
         it('Should be instance of Shapes', () => {
             const shape = new Shape();
+
             expect(shape).toBeInstanceOf(Shape);
         })
     })
@@ -22,22 +23,32 @@ describe('Circle', () => {
 
 describe('Circle', () => {
     describe('Circle svg', () => {
-                                    it('Should be circle svg with selected color', () => {
-                                        const circle = new Circle();
-                                        let color = 'blue';
-                                        circle.setColor("blue");
-                                        expect(circle.render()).toEqual(`<circle cx="150" cy="100" r="80" fill="${color}"/>`);
-                                    })
-                                })
-                            })
+        it('Should be circle svg with selected color', () => {
+            const circle = new Circle();
+            let color = 'blue';
+            circle.setColor("blue");
+            expect(circle.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+            <circle cx="150" cy="100" r="80" fill="${color}" />
+          
+            <text x="150" y="115" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+          
+          </svg>`);
+        })
+    })
+})
+                                    
 
 describe('Square', () => {
     describe('Square svg', () => {
         it('Should be square svg with selected color', () => {
             const square = new Square();
-            let color = 'pink'
+            let color = ('pink')
             square.setColor(color);
-            expect(square.render()).toEqual(`<rect x="50" y="65" width="150" height="150" fill="${color}"/>`);
+            expect(square.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+            <rect x="50" y="65" width="150" height="150" fill="${color}"/>
+            <text x="125" y="150" font-size="50" text-anchor="middle" fill="${this.textColor}">${this.text}</text> </svg>`);
            })
     })
 })
@@ -48,7 +59,11 @@ describe('Triangle', () => {
             const triangle = new Triangle();
             let color = 'yellow';
             triangle.setColor(color);
-            expect(triangle.render()).toEqual(`<polygon points="150, 18 244, 182 56, 182" fill="${color}" />`);
+            expect(triangle.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+            <polygon points="150, 18 244, 182 56, 182" fill="${color}" />
+            <text x="150" y="130" font-size="40" text-anchor="middle" fill="${this.textColor}">${this.text}</text> </svg>`
+    );
            })
     })
 })
